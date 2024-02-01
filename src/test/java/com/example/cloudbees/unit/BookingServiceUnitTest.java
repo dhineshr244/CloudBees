240ab20e7a502e6ddd206aab966dd3fe9925b769
dhinesh <dhineshr244@gmail.com>
2024-02-01 23:48:15 +0530

    commit

--- src/test/java/com/example/cloudbees/unit/BookingServiceUnitTest.java
+++ src/test/java/com/example/cloudbees/unit/BookingServiceUnitTest.java
@@ -3,19 +3,12 @@ package com.example.cloudbees.unit;
 import com.example.cloudbees.dto.BookingDetails;
 import com.example.cloudbees.repo.BookingDetailsRepository;
 import com.example.cloudbees.repo.SeatDetailsRepository;
-import com.example.cloudbees.service.BookingService;
 import com.example.cloudbees.service.impl.BookingServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static io.restassured.RestAssured.when;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doNothing;
 
 @ExtendWith(MockitoExtension.class)
 public class BookingServiceUnitTest {
